8900104e5f6535f9d09f8ba4d261f419899f6bcc
Gabriel Jiménez <gabjisa3420@gmail.com>
2022-02-02 13:59:32 -0600

    Implementación ejercicio 1

---
+++ EjerciciosHackerRank/QuintaPráctica/Solution.java
@@ -0,0 +1,107 @@
+import java.util.Scanner;
+
+/**
+ * Prints the n-th line of Pascal's triangle for (a-b)^n
+ */
+public class Solution
+{
+	/**
+	 * Gets data from standard input
+	 */
+	private Scanner input = null;
+
+	/**
+	 * Start the execution of the solution
+	 * @param args Command line arguments
+	 */
+	public static void main(String[] args)
+	{
+		Solution solution = new Solution();
+		solution.run();
+	}
+
+	/**
+	 * Run the solution. This method is called from main()
+	 */
+	public void run()
+	{
+		// Create object to read data from standard input
+		this.input = new Scanner(System.in);
+
+		// This code replicates the input to the standard output
+		// Modify this code to solve the problem
+		while ( this.input.hasNextLine() )
+		{
+			int grado = this.input.nextInt();
+
+            if (grado < 0) {
+                System.out.printf("%d: invalid data", grado);
+            } else {
+                grado = n;
+
+                System.out.printf("%d: ", n);
+                //Imprime polinomio
+                for (int i = 0; i < n; i++) {
+                    imprimirTermino(i, n);
+                }
+            }
+		}
+		// Close the standard input
+		this.input.close();
+	}
+
+    public int factorial (int natural) {
+        if (natural == 0) {
+            return 1;
+        } else {
+            int j = 1;
+            int factorial = 1;
+            while (j <= natural) {
+                factorial *= j;
+                j++;
+            }
+            return factorial;
+        }
+    }
+
+    public int[] filaPascal(int n) {
+        int[] filaPascal = new int[n+1];
+        for (int j = 0; j <= n; j++) {
+            filaPascal[j] = factorial(n)/(factorial(j)*factorial(n-j));
+        }
+        return filaPascal;
+    }
+
+    public void imprimirTermino(int i, int n) {
+        
+        int[] coefVector = filaPascal(n);
+        int coeficiente = coefVector[i];
+
+        if (i == 0) {
+            if (n == 0) {
+                System.out.println("1");
+            } else if (n == 1) {
+                System.out.print("a");
+            } else {
+                System.out.printf("a^%d", n);
+            }
+        } else if (i == n) {
+            if (n == 1) {
+                System.out.println(" - b");
+            } else {
+                System.out.printf(" - b^%d%n", n);
+            }
+        } else {
+            String signo = (i%2==0)?"+":"-";
+            if ((n-i)==1 && i==1) {
+                System.out.printf(" %s %dab", signo, coeficiente);
+            } else if ((n-i) == 1) {
+                System.out.printf(" %s %dab^%d", signo, coeficiente, i);
+            } else if (i == 1) {
+                System.out.printf(" %s %da^%db", signo, coeficiente, n-i);
+            } else {
+                System.out.printf(" %s %da^%db^%d", signo, coeficiente, n-i, i);
+            }
+        }
+    }
+}
