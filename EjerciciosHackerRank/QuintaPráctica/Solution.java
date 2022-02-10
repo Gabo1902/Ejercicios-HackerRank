96e0b553d32108efa29398d7b2b16d33f3324e45
Gabriel Jimenez <gabjisa3420@gmail.com>
2022-02-10 04:15:12 -0600

    Commit de implementación

--- EjerciciosHackerRank/QuintaPráctica/Solution.java
+++ EjerciciosHackerRank/QuintaPráctica/Solution.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 /**
  * Validates a treasure map and prints the path from a starting
@@ -31,12 +32,36 @@ public class Solution
 
 		// This code replicates the input to the standard output
 		// Modify this code to solve the problem
-		while ( this.input.hasNextLine() )
-		{
-			System.out.println( this.input.nextLine() );
-		}
+		int numFilas = this.input.nextInt();
+    int numColumnas = this.input.nextInt();
+    int filaDeInicio = this.input.nextInt();
+    int columnaDeInicio = this.input.nextInt();
+    
+    int[][] mapaOculto = new int[numFilas][numColumnas];
+
+    //Lee la matriz oculta (lo hace bien)
+    for (int i = 0; i < numFilas; i++) {
+      for (int j = 0; j < numColumnas; j++) {
+        mapaOculto[i][j] = this.input.nextInt();
+      }
+    }
 
 		// Close the standard input
 		this.input.close();
 	}
 }
+
+/*
+
+for(int i = 0; i < numFilas; i++) {
+      for (int j = 0; j < numColumnas; j++) {
+        if(j != numColumnas-1) {
+          System.out.printf("%3d ", mapaOculto[i][j]);
+        } else {
+          System.out.printf("%3d", mapaOculto[i][j]);
+        }
+      }
+      System.out.println();
+    }
+
+*/
